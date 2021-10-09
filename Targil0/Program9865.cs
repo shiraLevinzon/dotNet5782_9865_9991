f28a32fdfcd3e57a4621a5d277af14604ec8bfff
שירה לוינזון <levinzon@g.jct.ac.il>
2021-10-09 23:22:36 +0300

    inital code+ welcome functions

---
+++ Targil0/Program9865.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Targil0
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            Welcome9865();
+            Welcome9991();
+            Console.ReadKey();
+        }
+
+        private static void Welcome9865()
+        {
+            string name;
+            Console.WriteLine("Enter your name: ");
+            name = Console.ReadLine();
+            Console.WriteLine("{0}, welcome to my first console application", name);
+        }
+        static partial void Welcome9991();
+    }
+}

4cda9b8d14ed3a86e4b252933be32d0f59d8e703
שירה לוינזון <levinzon@g.jct.ac.il>
2021-10-23 21:26:22 +0300

    targil 1- first push

---
+++ DAL/DO/BaseStation.cs
@@ -0,0 +1,23 @@
+﻿using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+using IDAL.DO;
+
+namespace IDAL
+{
+    namespace DO
+    {
+        public struct BaseStation
+        {
+            public int ID { get; set; }
+
+
+
+
+        }
+    }
+    
+}

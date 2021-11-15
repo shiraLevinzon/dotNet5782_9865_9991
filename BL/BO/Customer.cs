a86e80242a9470b8263e4e92260254e008a13c32
שירה לוינזון <levinzon@g.jct.ac.il>
2021-11-15 19:38:59 +0200

    'בWE

---
+++ BL/BO/Customer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace BL.BO
+{
+    class Customer
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Phone { get; set; }
+        public Location ParcelLocation { get; set; }
+        public List<Parcel> PackagesFromCustomer { get; set; }
+        public List<Parcel> PackagesToCustomer { get; set; }
+
+    }
+}
